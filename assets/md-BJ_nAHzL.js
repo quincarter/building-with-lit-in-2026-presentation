d1ed8bd1c4ab626f6bb50633f897f07c397c02aa
Quin Carter <quin.carter@gmail.com>
2026-05-09 22:16:14 -0400

    Updates

---
+++ assets/md-BJ_nAHzL.js
@@ -0,0 +1,15 @@
+import{L as e,T as t,X as n,_ as r,_t as i,b as a,g as o,ht as s,x as c}from"./modules/shiki-D2nAf9zx.js";import{n as l,t as u}from"./slidev/context-CQvdRSsH.js";import{t as d}from"./slidev/CodeBlockWrapper-DU5SXOwU.js";import{t as f}from"./slidev/two-cols-CCkZvvSn.js";var p={__name:`slides.md__slidev_5`,setup(p){let{$slidev:m,$nav:h,$clicksContext:g,$clicks:_,$page:v,$renderContext:y,$frontmatter:b}=l();return g.setup(),(l,p)=>{let m=d;return e(),r(f,i(t(s(u)(s(b),4))),{right:n(e=>[c(m,{title:``,ranges:[]},{default:n(()=>[...p[0]||=[o(`pre`,{class:`shiki shiki-themes vitesse-dark vitesse-light slidev-code`,style:{"--shiki-dark":`#dbd7caee`,"--shiki-light":`#393a34`,"--shiki-dark-bg":`#121212`,"--shiki-light-bg":`#ffffff`}},[o(`code`,{class:`language-text`},[o(`span`,{class:`line`},[o(`span`,null,`src`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`├── app-shell.ts`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`├── components`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   ├── card`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   ├── chart-js`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   ├── header`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   └── todos`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`├── shared`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   ├── configuration`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   ├── contexts`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   ├── stores`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`│   └── utilities`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`└── views`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`    ├── home-page`)]),a(`
+`),o(`span`,{class:`line`},[o(`span`,null,`    └── todos-page`)])])],-1)]]),_:1})]),default:n(()=>[p[1]||=o(`h1`,null,`Application Structure`,-1),p[2]||=o(`p`,null,`Our "App Shell" architecture:`,-1),p[3]||=o(`ul`,null,[o(`li`,null,[o(`strong`,null,`src/app-shell.ts`),a(`: Main entry & layout`)]),o(`li`,null,[o(`strong`,null,`components/`),a(`: Atomic UI (Cards, Charts, Todos)`)]),o(`li`,null,[o(`strong`,null,`views/`),a(`: Page-level components & Mixins`)]),o(`li`,null,[o(`strong`,null,`shared/`),a(`: `),o(`ul`,null,[o(`li`,null,[o(`strong`,null,`configuration/`),a(`: Routes & MFE config`)]),o(`li`,null,[o(`strong`,null,`contexts/`),a(`: Auth, Nav, MFE loaders`)]),o(`li`,null,[o(`strong`,null,`stores/`),a(`: Signal-based state`)]),o(`li`,null,[o(`strong`,null,`utilities/`),a(`: Shared logic`)])])])],-1)]),_:1},16)}}};export{p as default};
